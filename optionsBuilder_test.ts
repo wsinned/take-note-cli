ac6e850f1c1a36494355422de0a2b029f4b0184d
Dennis Woodruff <denniswoodruffuk@gmail.com>
2025-04-29 15:07:01 +0000

    Adds tests for multiple boolean options

--- optionsBuilder_test.ts
+++ optionsBuilder_test.ts
@@ -1,6 +1,5 @@
 import { assertArrayIncludes, assertEquals } from "@std/assert";
 import { buildHelp, buildOptions, option } from "./optionsBuilder.ts";
-import type { ParseOptions } from "@std/cli/parse-args";
 
 const helpOption: option = {
   name: "Help",
@@ -10,15 +9,17 @@ const helpOption: option = {
   optionType: "boolean"
 }
 
+const verboseOption: option = {
+  name: "Verbose",
+  shortKey: "-v",
+  longKey: "--verbose",
+  helpText: "Provides verbose log output",
+  optionType: "boolean"
+}
+
 Deno.test("It builds empty help text from empty options", () => {
   const expected: string[] = []
   const help = buildHelp([])
-  assertArrayIncludes(help, expected)
-});
-
-Deno.test("It builds help text from a single boolean `option", () => {
-  const expected = ["Help: --help, -h Provides help on the options"]
-  const help = buildHelp([helpOption])
   assertArrayIncludes(help, expected)
 });
 
@@ -28,16 +29,41 @@ Deno.test("It builds an empty option", () => {
   assertEquals(ops.boolean, expected)
 });
 
-Deno.test("It builds a single boolean `option", () => {
+Deno.test("It builds help text from a single boolean option", () => {
+  const expected = ["Help: --help, -h Provides help on the options"]
+  const help = buildHelp([helpOption])
+  assertArrayIncludes(help, expected)
+});
+
+Deno.test("It builds a single boolean option", () => {
   const expected = [ "--help"]
   const ops = buildOptions([helpOption])
   assertEquals(ops.boolean, expected)
 });
 
-Deno.test("It builds a single aliased boolean `option", () => {
+Deno.test("It builds a single aliased boolean option", () => {
   const expected = {"--help": "-h"}
   const ops = buildOptions([helpOption])
-  // console.log(`option: ${Deno.inspect(ops.alias)}`)
   assertEquals(ops.alias, expected)
 });
 
+Deno.test("It builds help text from multiple boolean options", () => {
+  const expected = [
+    "Help: --help, -h Provides help on the options",
+    "Verbose: --verbose, -v Provides verbose log output"
+  ]
+  const help = buildHelp([helpOption, verboseOption])
+  assertArrayIncludes(help, expected)
+});
+
+Deno.test("It builds multiple boolean options", () => {
+  const expected = [ "--help", "--verbose"]
+  const ops = buildOptions([helpOption, verboseOption])
+  assertEquals(ops.boolean, expected)
+});
+
+Deno.test("It builds multiple aliased boolean options", () => {
+  const expected = {"--help": "-h", "--verbose": "-v"}
+  const ops = buildOptions([helpOption, verboseOption])
+  assertEquals(ops.alias, expected)
+});
